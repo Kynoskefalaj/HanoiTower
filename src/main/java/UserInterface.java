6c26255d209f536adef15647ce9615d028ec0ba3
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-03-08 23:15:40 +0100

    add buttons

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -5,9 +5,12 @@ public class UserInterface {
     public JFrame window;
     public Container con;
     public JPanel leftStickPanel, middleStickPanel, rightStickPanel, buttonPanel;
+    public JButton leftButton, middleButton, rightButton;
     int windowX = 1400;
     int windowY = 800;
     int marginX, marginY, innerMargin, stickPanelWidth, stickPanelHeight;
+
+    public Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
 
     public UserInterface(){
         displayWindow();
@@ -16,7 +19,7 @@ public class UserInterface {
 
     protected void displayWindow(){
         window = new JFrame(); //creating main window as an instance of JFrame
-        window.setSize(windowX + 20, windowY + 30);
+        window.setSize(windowX + 13, windowY + 36);
         window.getContentPane().setBackground(Color.BLACK);
         window.setVisible(true);
         window.setLayout(null);
@@ -49,10 +52,29 @@ public class UserInterface {
         rightStickPanel.setBounds(marginX + 2 * (innerMargin + stickPanelWidth), marginY, stickPanelWidth, stickPanelHeight);
         rightStickPanel.setBackground(Color.magenta);
 
+
+        leftButton = new JButton("LEFT");
+        leftButton.setBackground(Color.yellow);
+        leftButton.setForeground(Color.black);
+        leftButton.setFont(normalFont);
+        buttonPanel.add(leftButton);
+
+        middleButton = new JButton("MIDDLE");
+        middleButton.setBackground(Color.cyan);
+        middleButton.setForeground(Color.black);
+        middleButton.setFont(normalFont);
+        buttonPanel.add(middleButton);
+
+        rightButton = new JButton("RIGHT");
+        rightButton.setBackground(Color.magenta);
+        rightButton.setForeground(Color.black);
+        rightButton.setFont(normalFont);
+        buttonPanel.add(rightButton);
+        
+
         con.add(buttonPanel);
         con.add(leftStickPanel);
         con.add(middleStickPanel);
         con.add(rightStickPanel);
-
     }
 }
